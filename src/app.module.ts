eb1a61236f74db1b7e650475261f3c52f5a40c48
jangseunghoon <jsh15932@noutecompany.com>
2022-02-11 11:28:13 +0900

    DB 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,19 @@ import { UserModule } from './user/user.module';
 import { ChatModule } from './chat/chat.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: process.env.DB_HOST,
       port: 3306,
-      username: 'root',
-      password: 'pw12345678',
-      database: 'chat',
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
       synchronize: true,
       entities: [
         "dist/**/*.entity{.ts,.js}"
